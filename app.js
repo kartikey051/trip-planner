49844f5efa7a8f48b173e621baed28faf1e601e5
Kartikey <kartikeysingh051@gmail.com>
2023-02-21 10:31:58 +0530

    app.js and style.css added

---
+++ app.js
@@ -0,0 +1,10 @@
+//set map options
+var myLatLng ={lat: 20.278940,lng:85.731682};
+var mapOptions = {
+    center: myLatLng,
+    zoom: 7,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+
+};
+//create map
+var map= new google.maps.MAP(document.getElementById("googleMap"),mapOptions)
